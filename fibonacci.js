86293306c093b466dd8a3df91f44e25c242d01de
Yeamin Adib <yeaminadib@gmail.com>
2021-01-21 23:39:31 +0600

    Function Itteration Method - Fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -1,10 +1,16 @@
-var fibo = [0, 1];
-// fibo[2] = fibo[2-1] + fibo[2-2] 
-// fibo[3] = fibo[3-1] + fibo[3-2] 
-// fibo[4] = fibo[4-1] + fibo[4-2] 
-// fibo[5] = fibo[5-1] + fibo[5-2] 
-// fibo[i] = fibo[i-1] + fibo[i-2] 
-for (var i = 2; i<=10; i++){
-    fibo[i] = fibo [i-1] + fibo[i-2];
+    // fibo[2] = fibo[2-1] + fibo[2-2] 
+    // fibo[3] = fibo[3-1] + fibo[3-2] 
+    // fibo[4] = fibo[4-1] + fibo[4-2] 
+    // fibo[5] = fibo[5-1] + fibo[5-2] 
+    // fibo[i] = fibo[i-1] + fibo[i-2] 
+function fibonacci(n){
+    var fibo = [0, 1];
+    for (var i = 2; i<=10; i++){
+        fibo[i] = fibo [i-1] + fibo[i-2];
+    }
+    return fibo;
 }
-console.log(fibo);
+  
+ var res = fibonacci(12);
+ console.log(res);
+
